862e6bb72873c9c29910d96ad4ca7ea1bb332b8a
Alexandru Botez <alexandru.botez@ddroidd.com>
2023-11-15 10:59:26 +0200

    Intial project setup

---
+++ src/test/java/com/example/bequiz/BeQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bequiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

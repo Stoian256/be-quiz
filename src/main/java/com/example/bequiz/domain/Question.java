96b30ef411dabae338fac0b07903150963fcf686
Silviu Stoian <silviu.stoian@ddroidd.com>
2023-11-17 12:03:08 +0200

    Refactor entities, created BaseEntity for common fields

--- src/main/java/com/example/bequiz/domain/Question.java
+++ src/main/java/com/example/bequiz/domain/Question.java
@@ -2,37 +2,26 @@ package com.example.bequiz.domain;
 
 import com.example.bequiz.utils.Difficulty;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
-@Builder
 @Data
-@Entity
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
+@Entity
 @Table(name = "question")
-public class Question {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+public class Question extends BaseEntity {
 
     private String questionTitle;
 
     private Difficulty difficultly;
 
-    @OneToMany(mappedBy = "question")
+    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
     private List<Answer> answers;
 
     @ManyToMany
@@ -42,19 +31,4 @@ public class Question {
             inverseJoinColumns = @JoinColumn(name = "tag_id")
     )
     private List<Tag> tags;
-
-    @CreatedDate
-    private LocalDateTime dateCreated;
-
-    @CreatedBy
-    private String createdBy;
-
-    @LastModifiedDate
-    private LocalDateTime dateLastModified;
-
-    @LastModifiedBy
-    private String lastModifiedBy;
-
-    private boolean isDeleted;
-
 }
--- src/main/java/com/example/bequiz/domain/Tag.java
+++ src/main/java/com/example/bequiz/domain/Tag.java
@@ -2,62 +2,26 @@ package com.example.bequiz.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
-@Builder
+
 @Data
-@Entity
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 @Table(name = "tag")
-@EntityListeners(AuditingEntityListener.class)
-public class Tag {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+public class Tag extends BaseEntity {
 
     private String tagTitle;
 
     @JsonIgnore
+    @ToString.Exclude
     @ManyToMany(mappedBy = "tags")
     private List<Question> questions;
-
-    @CreatedDate
-    private LocalDateTime dateCreated;
-
-    @CreatedBy
-    private String createdBy;
-
-    @LastModifiedDate
-    private LocalDateTime dateLastModified;
-
-    @LastModifiedBy
-    private String lastModifiedBy;
-
-    private boolean isDeleted;
-
-    @Override
-    public String toString() {
-        return "Tag{" +
-                "id=" + id +
-                ", tagTitle='" + tagTitle + '\'' +
-                ", dateCreated=" + dateCreated +
-                ", createdBy='" + createdBy + '\'' +
-                ", dateLastModified=" + dateLastModified +
-                ", lastModifiedBy='" + lastModifiedBy + '\'' +
-                ", isDeleted=" + isDeleted +
-                '}';
-    }
 }

13c0736a9e867a9a57d779d0db99603c00e5b203
Silviu Stoian <silviu.stoian@ddroidd.com>
2023-11-20 10:18:02 +0200

    Changed question field questionTitle to questionBody

--- src/main/java/com/example/bequiz/domain/Question.java
+++ src/main/java/com/example/bequiz/domain/Question.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Table(name = "question")
 public class Question extends BaseEntity {
 
-    private String questionTitle;
+    private String questionBody;
 
     private Difficulty difficultly;
 

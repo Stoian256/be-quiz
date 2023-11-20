4ad17984403408adb52e17ffc6d9efbb84205f69
Alexandru Botez <alexandru.botez@ddroidd.com>
2023-11-20 10:18:53 +0200

    I have finished the Security Setup task

---
+++ src/main/java/com/example/bequiz/configuration/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package com.example.bequiz.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+
+@Configuration
+public class ApplicationConfig implements WebMvcConfigurer {
+
+    private final ApplicationProperties applicationProperties;
+
+    @Autowired
+    public ApplicationConfig(ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(applicationProperties.getClientOriginUrl())
+                .allowedHeaders(HttpHeaders.AUTHORIZATION, HttpHeaders.CONTENT_TYPE)
+                .allowedMethods(HttpMethod.GET.name())
+                .maxAge(86400);
+    }
+}
--- src/main/java/com/example/bequiz/controller/QuizController.java
+++ src/main/java/com/example/bequiz/controller/QuizController.java
@@ -1,8 +1,19 @@
 package com.example.bequiz.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuizController {
+
+
+    @ResponseBody
+    @GetMapping("/")
+    @PreAuthorize("hasAuthority('Admin')")
+    public String helloWorld(){
+        return "hello world!";
+    }
 
 }

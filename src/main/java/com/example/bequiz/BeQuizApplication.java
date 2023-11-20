2fc7c1e08d7253670163775074e77d66ba574c02
Alexandru Botez <alexandru.botez@ddroidd.com>
2023-11-20 10:18:53 +0200

    I have finished the Security Setup task

--- src/main/java/com/example/bequiz/BeQuizApplication.java
+++ src/main/java/com/example/bequiz/BeQuizApplication.java
@@ -2,14 +2,16 @@ package com.example.bequiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableJpaAuditing()
 public class BeQuizApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BeQuizApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BeQuizApplication.class, args);
+	}
 
 }
---
+++ src/main/java/com/example/bequiz/configuration/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.bequiz.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated()
+                ).cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt->jwt.jwtAuthenticationConverter(makePermissionsConverter())));
+        return httpSecurity.build();
+    }
+
+    private JwtAuthenticationConverter makePermissionsConverter() {
+        final var jwtAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtAuthoritiesConverter.setAuthoritiesClaimName("permissions");
+        jwtAuthoritiesConverter.setAuthorityPrefix("");
+
+        final var jwtAuthConverter = new JwtAuthenticationConverter();
+        jwtAuthConverter.setJwtGrantedAuthoritiesConverter(jwtAuthoritiesConverter);
+
+        return jwtAuthConverter;
+    }
+}

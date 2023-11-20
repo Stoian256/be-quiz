df0498d7031d9b0831624e91b36a1a78d5181c6a
Alexandru Botez <alexandru.botez@ddroidd.com>
2023-11-20 10:18:53 +0200

    I have finished the Security Setup task

--- src/main/java/com/example/bequiz/BeQuizApplication.java
+++ src/main/java/com/example/bequiz/BeQuizApplication.java
@@ -2,8 +2,10 @@ package com.example.bequiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BeQuizApplication {
 
 	public static void main(String[] args) {

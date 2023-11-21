b222c2987b948592df5480c9e2344783f99daedc
Alexandru Botez <alexandru.botez@ddroidd.com>
2023-11-21 11:33:34 +0200

    I have created the Create Question Endpoint

--- src/main/java/com/example/bequiz/controller/QuestionController.java
+++ src/main/java/com/example/bequiz/controller/QuestionController.java
@@ -1,13 +1,22 @@
 package com.example.bequiz.controller;
 
+import com.example.bequiz.dto.CreateQuestionDTO;
 import com.example.bequiz.service.QuestionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/questions")
 public class QuestionController {
     private final QuestionService questionService;
+
+
+
+    @PostMapping("/createQuestion")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createQuestion(@RequestBody CreateQuestionDTO createQuestionDTO){
+        questionService.createQuestion(createQuestionDTO);
+    }
 }
--- src/main/java/com/example/bequiz/repository/TagRepository.java
+++ src/main/java/com/example/bequiz/repository/TagRepository.java
@@ -8,4 +8,6 @@ import java.util.UUID;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, UUID> {
+
+    Tag findByTagTitle(String tagTitle);
 }

74e281ea77e09357ad917a2d3d02b29a9b95b6d9
Silviu Stoian <silviu.stoian@ddroidd.com>
2023-11-21 11:54:06 +0200

    Added default sort DESC by dateLastModified and dateCreated

--- src/main/java/com/example/bequiz/service/QuestionService.java
+++ src/main/java/com/example/bequiz/service/QuestionService.java
@@ -11,6 +11,7 @@ import com.example.bequiz.utils.EntitiesMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -36,8 +37,12 @@ public class QuestionService {
         if (questionFilters.getItemsPerPage() != null && questionFilters.getPageIndex() != null)
             pageRequest = PageRequest.of(questionFilters.getPageIndex(), questionFilters.getItemsPerPage());
 
+        Sort defaultSort = Sort.by(Sort.Direction.DESC, "dateLastModified", "dateCreated");
+
+
         return questionRepository
-                .findAll(questionBooleanBuilder.buildBooleanFromQuestionFilters(questionFilters), pageRequest)
+                .findAll(questionBooleanBuilder.buildBooleanFromQuestionFilters(questionFilters),
+                        pageRequest.withSort(defaultSort))
                 .map(entitiesMapper::questionToQuestionDTO);
     }
 }

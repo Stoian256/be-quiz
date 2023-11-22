d7d9d8621065f63a327de171274a775fbdb4d3ff
Alexandru Botez <alexandru.botez@ddroidd.com>
2023-11-22 11:57:33 +0200

    I have created the Edit Question Endpoint

--- src/main/java/com/example/bequiz/controller/QuestionController.java
+++ src/main/java/com/example/bequiz/controller/QuestionController.java
@@ -55,4 +55,5 @@ public class QuestionController {
     public void updateQuestion(@PathVariable UUID id,@RequestBody CreateQuestionDTO createQuestionDTO){
        questionService.editQuestion(id,createQuestionDTO);
     }
+
 }
--- src/main/java/com/example/bequiz/service/QuestionService.java
+++ src/main/java/com/example/bequiz/service/QuestionService.java
@@ -133,4 +133,56 @@ public class QuestionService {
         }
         return false;
     }
+    private final TagRepository tagRepository;
+
+    @Transactional
+    public List<Tag> processTags(List<String> tagList) {
+        return tagList.stream()
+                .map(string -> {
+                    String trimmedString = string.trim();
+                    if (trimmedString.length() > 1) {
+                        return trimmedString.substring(0, 1).toUpperCase() + trimmedString.substring(1).toLowerCase();
+                    } else {
+                        return trimmedString;
+                    }
+                })
+                .map(tagTitle -> {
+                    Tag existingTag = tagRepository.findByTagTitle(tagTitle);
+                    if (existingTag == null) {
+                        existingTag = new Tag(tagTitle, null);
+                        tagRepository.save(existingTag);
+                    }
+                    return existingTag;
+                }).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void editQuestion(UUID uuid, CreateQuestionDTO createQuestionDTO) {
+        Question question = questionRepository.findById(uuid).orElseThrow(() -> new ObjectNotFoundException(uuid, "Question not found"));
+        if (!validateAnswers(createQuestionDTO.getAnswers())){
+            throw new RuntimeException("There must be at least 2 answers and one of them must be correct");
+        }
+        question.setAnswers(createQuestionDTO.getAnswers());
+        question.setDifficultly(Difficulty.valueOf(createQuestionDTO.getDifficultly()));
+        question.setQuestionBody(createQuestionDTO.getQuestionBody());
+        question.setTags(processTags(createQuestionDTO.getTags()));
+
+        questionRepository.save(question);
+    }
+
+    public Question findQuestionById(UUID id) {
+        return questionRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(id, "Question not found! "));
+    }
+
+    public boolean validateAnswers(List<Answer> answers) {
+        if (answers.size() < 2) {
+            return false;
+        }
+        for (Answer answer : answers) {
+            if (answer.isCorrectAnswer()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

b8a38e50c60eca5a171ef7aeca687c13845d272d
Alexandru Botez <alexandru.botez@ddroidd.com>
2023-11-24 11:48:45 +0200

    I have created the first part of the feature

--- src/main/java/com/example/bequiz/domain/Question.java
+++ src/main/java/com/example/bequiz/domain/Question.java
@@ -4,7 +4,6 @@ import com.example.bequiz.utils.Difficulty;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
-import net.minidev.json.annotate.JsonIgnore;
 
 import java.util.List;
 
